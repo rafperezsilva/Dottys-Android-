19266fe50bd6bb8491793574cf13957153beb240
Rafael <rafperezsi@gmail.com>
2020-03-10 11:14:20 -0400

    Login Init Repository

---
+++ app/src/main/java/com/keylimetie/dottys/splash/DottysSplashPagerAdapter.kt
@@ -0,0 +1,77 @@
+package com.keylimetie.dottys.splash
+
+
+  import android.content.Context
+  import android.util.DisplayMetrics
+  import android.view.LayoutInflater
+  import android.view.View
+  import android.view.ViewGroup
+  import android.widget.ImageView
+  import android.widget.LinearLayout
+  import android.widget.TableRow
+  import android.widget.TextView
+   import androidx.viewpager.widget.PagerAdapter
+  import com.keylimetie.dottys.R
+  import kotlin.math.roundToInt
+
+class CustomPagerAdapter(private val mContext: Context, val list: ArrayList<DottysPagerModel>, var metrics: DisplayMetrics? = null) : PagerAdapter() {
+
+
+    override fun instantiateItem(collection: ViewGroup, position: Int): Any {
+        val modelObject = list[position]
+        val inflater = LayoutInflater.from(mContext)
+        val layout = inflater.inflate(R.layout.splash_pager_item, collection, false) as ViewGroup
+        val image = layout.findViewById<ImageView>(R.id.image_pager)
+        val imageTop = layout.findViewById<ImageView>(R.id.image_top_pager)
+        val title = layout.findViewById<TextView>(R.id.title_pager)
+        val subtitle = layout.findViewById<TextView>(R.id.subtitle_pager)
+        var layoutParams = TableRow.LayoutParams(metrics!!.widthPixels, metrics!!.heightPixels / 2)
+        image.layoutParams = layoutParams
+
+        when (position){
+            0 -> {
+                title.height = 0
+            }
+            1,2,3 -> {
+                val param = title.layoutParams as LinearLayout.LayoutParams
+                param.setMargins(25,80,25,130)
+                title.layoutParams = param
+                imageTop.layoutParams.height = 0
+                when (position){
+
+                    3 -> {
+                        param.setMargins(25,80,25,75)
+                        title.layoutParams = param
+                        imageTop.layoutParams.height = 0
+                        layoutParams = TableRow.LayoutParams((metrics!!.widthPixels * 0.8).roundToInt(), metrics!!.heightPixels / 2)
+                        layoutParams.topMargin = 0
+                        image.layoutParams = layoutParams
+
+                    }
+                }
+            }
+            else -> {
+                imageTop.layoutParams.height = 0
+            }
+        }
+
+        image.setImageResource(modelObject.image)
+        title.text = list[position].title
+        subtitle.text = list[position].subtitle
+        collection.addView(layout)
+        return layout
+    }
+    override fun destroyItem(collection: ViewGroup, position: Int, view: Any) {
+        collection.removeView(view as View)
+    }
+    override fun getCount(): Int {
+        return list.count()
+    }
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+//    override fun getPageTitle(position: Int): CharSequence {
+//       // val customPagerEnum = Model.values()[position]
+//        //return mContext.getString(customPagerEnum.titleResId)
+//    }
+}
---
+++ app/src/main/java/com/keylimetie/dottys/splash/DottysSplashViewModel.kt
@@ -0,0 +1,80 @@
+package com.keylimetie.dottys.splash
+
+import android.app.Activity
+import android.content.Intent
+import android.util.DisplayMetrics
+import android.widget.Button
+import androidx.lifecycle.ViewModel
+import androidx.viewpager.widget.ViewPager
+import com.keylimetie.dottys.R
+import com.keylimetie.dottys.login.DottysLoginActivity
+import com.viewpagerindicator.CirclePageIndicator
+
+
+class DottysSplashViewModel : ViewModel() {
+    private lateinit var viewPager: ViewPager
+    private lateinit var loginButton: Button
+    private lateinit var sigupButton: Button
+    private val displayMetrics = DisplayMetrics()
+
+     fun initView(context: Activity){
+        viewPager = context.findViewById(R.id.splashViewPager)
+        loginButton = context.findViewById(R.id.login_splash_button)
+        sigupButton = context.findViewById(R.id.sign_up_splash_button)
+        context.windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+         val indicator =  context.findViewById(R.id.indicator) as CirclePageIndicator
+         viewPager.adapter = CustomPagerAdapter(
+             context,
+             initSplashData(context),
+             displayMetrics
+         )
+         indicator.setViewPager(viewPager)
+         buttonLisener(context)
+    }
+
+    fun initSplashData(context:Activity):ArrayList<DottysPagerModel>{
+        var firstSplash = DottysPagerModel(
+            "",
+            R.mipmap.dottys_splash_first_image,
+            context.getString(R.string.splash_first_text)
+        )
+        var secondSplash = DottysPagerModel(
+            context.getString(R.string.title_second_top_splas),
+            R.mipmap.dottys_splash_second_image,
+            context.getString(R.string.splash_second_text)
+        )
+        var thirdSplash = DottysPagerModel(
+            context.getString(R.string.title_third_splash_item),
+            R.mipmap.dottys_splash_third_image,
+            context.getString(R.string.subtitle_third_splash_item)
+        )
+        var fourSplash = DottysPagerModel(
+            context.getString(R.string.title_four_splash_item),
+            R.mipmap.dottys_splash_four_image,
+            context.getString(
+                R.string.subtitle_four_splash_item
+            )
+        )
+        var listData = ArrayList<DottysPagerModel>()
+        listData.add(firstSplash)
+        listData.add(secondSplash)
+        listData.add(thirdSplash)
+        listData.add(fourSplash)
+        return  listData
+    }
+
+
+    fun buttonLisener(context:Activity){
+        loginButton.setOnClickListener {
+            val intentLogin = Intent(context, DottysLoginActivity::class.java)
+            context.startActivity(intentLogin)
+        }
+
+        sigupButton.setOnClickListener {
+
+        }
+    }
+
+
+}
